1b2c22abac687e1323564c8224a86bdeebdeecf2
juniordefarias <defariasjunior753@gmail.com>
2022-04-10 00:33:50 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function showNavMenu() {
+  const nav = document.getElementById('nav-menu');
+  nav.classList.toggle('show-menu');
+}
+
+/* const navToggle = document.getElementById('nav-toggle');
+navToggle.addEventListener('click', showNavMenu); */
